47a194ffd69554be6ec151df16f8082123b6da46
Topitops69 <topitops123@gmail.com>
2023-11-04 20:31:17 +0800

    Just the introduction part

--- app/src/main/java/com/example/cleartheshelves/MainActivity.java
+++ app/src/main/java/com/example/cleartheshelves/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.cleartheshelves;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.cleartheshelves.Miscellaneous.OptionsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView3 = findViewById(R.id.textView3);
+
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the OptionsActivity to display the list of options
+                Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
